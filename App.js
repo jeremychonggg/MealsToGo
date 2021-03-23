eb1026a68a36d785ad8b600470cb245c002a0caa
Jeremy Chong <h09083.cys@gmail.com>
2021-03-23 19:06:41 +0800

    Added Bottom Tab

--- App.js
+++ App.js
@@ -1,28 +1,29 @@
-import { StatusBar as ExpoStatusBar } from "expo-status-bar";
 import React from "react";
 import { Text } from "react-native";
+// Import 'Theme Setting'
 import { ThemeProvider } from "styled-components/native";
+// Import for 'Bottom Tab'
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+// Import for 'Icon'
 import { Ionicons } from "@expo/vector-icons";
+// Import for 'Status Bar'
+import { StatusBar as ExpoStatusBar } from "expo-status-bar";
 
+// Import 'Font Style'
 import {
   useFonts as useOswald,
   Oswald_400Regular,
 } from "@expo-google-fonts/oswald";
 import { useFonts as useLato, Lato_400Regular } from "@expo-google-fonts/lato";
 
+// Import 'Theme Setting'
 import { theme } from "./src/infrastructure/theme";
 import { RestaurantsScreen } from "./src/features/restaurants/screens/restaurants.screen";
 import { SafeArea } from "./src/components/utility/safe-area.component";
 
+// Create Bottom Tab
 const Tab = createBottomTabNavigator();
-
-const TAB_ICON = {
-  Restaurants: "md-restaurant",
-  Map: "md-map",
-  Settings: "md-settings",
-};
 
 const Settings = () => (
   <SafeArea>
@@ -35,6 +36,7 @@ const Map = () => (
   </SafeArea>
 );
 
+// Create Icon
 const createScreenOptions = ({ route }) => {
   const iconName = TAB_ICON[route.name];
   return {
@@ -44,6 +46,14 @@ const createScreenOptions = ({ route }) => {
   };
 };
 
+// Create Icon - Specify the Icon
+const TAB_ICON = {
+  Restaurants: "md-restaurant",
+  Map: "md-map",
+  Settings: "md-settings",
+};
+
+// Our App
 export default function App() {
   const [oswaldLoaded] = useOswald({
     Oswald_400Regular,
@@ -64,7 +74,7 @@ export default function App() {
           <Tab.Navigator
             screenOptions={createScreenOptions}
             tabBarOptions={{
-              activeTintColor: "tomato",
+              activeTintColor: "tomato", // Can customise with HEX Code
               inactiveTintColor: "gray",
             }}
           >
--- README.md
+++ README.md
@@ -2,19 +2,34 @@
 
 Packages:
 
-- Eslint
-- Prettier
-- React Native Paper
-  "yarn add react-native-paper"
-- Styled Components
-  "yarn add styled-components"
-- Expo Google Font
-  "expo install expo-font"
-  "expo install @expo-google-fonts/oswald"
-  // ^oswald is the font name you want
-- Expo SVG Handler
-  "expo install react-native-svg"
-- Navigation
-  "yarn add @react-navigation/native"
-  "expo install react-native-gesture-handler react-native-reanimated react-native-screens react-native-safe-area-context @react-native-community/masked-view"
-  "yarn add @react-navigation/bottom-tabs"
+1. Eslint
+2. Prettier
+
+3. React Native Paper
+
+   - "yarn add react-native-paper"
+
+4. Styled Components
+
+   - "yarn add styled-components"
+
+5. Expo Google Font
+
+   - "expo install expo-font"
+   - "expo install @expo-google-fonts/oswald"
+   - ^oswald is the font name you want
+
+6. Expo SVG Handler
+
+   - "expo install react-native-svg"
+
+7. Navigation
+   - "yarn add @react-navigation/native"
+8. Navigation Library
+   - "expo install react-native-gesture-handler react-native-reanimated react-native-screens react-native-safe-area-context @react-native-community/masked-view"
+9. Navigation Bottom Tabs Library
+
+   - "yarn add @react-navigation/bottom-tabs"
+
+10. Expo Icon
+    - Google Search @expo/vector-icons
